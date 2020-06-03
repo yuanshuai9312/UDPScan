2a53e1f1755e691c8a8b4bde921a1991b8535605
yuan.shuai <yuanshuai9312@gmail.com>
2020-06-04 01:18:02 +0900

    fist commit

---
+++ UDPScan/UDPScan.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace UDPScan
+{
+    partial class UDPScan
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbend = new System.Windows.Forms.TextBox();
+            this.tbstart = new System.Windows.Forms.TextBox();
+            this.tbipFrom = new System.Windows.Forms.TextBox();
+            this.listViewRes = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonUdp = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonTcp = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "PORT:  ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "IP:";
+            // 
+            // tbend
+            // 
+            this.tbend.Location = new System.Drawing.Point(237, 61);
+            this.tbend.Name = "tbend";
+            this.tbend.Size = new System.Drawing.Size(49, 21);
+            this.tbend.TabIndex = 11;
+            this.tbend.Text = "8085";
+            // 
+            // tbstart
+            // 
+            this.tbstart.Location = new System.Drawing.Point(134, 60);
+            this.tbstart.Name = "tbstart";
+            this.tbstart.Size = new System.Drawing.Size(41, 21);
+            this.tbstart.TabIndex = 10;
+            this.tbstart.Text = "8080";
+            // 
+            // tbipFrom
+            // 
+            this.tbipFrom.Location = new System.Drawing.Point(83, 33);
+            this.tbipFrom.Name = "tbipFrom";
+            this.tbipFrom.Size = new System.Drawing.Size(92, 21);
+            this.tbipFrom.TabIndex = 9;
+            this.tbipFrom.Text = "222.198.38.78";
+            // 
+            // listViewRes
+            // 
+            this.listViewRes.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listViewRes.FullRowSelect = true;
+            this.listViewRes.GridLines = true;
+            this.listViewRes.Location = new System.Drawing.Point(14, 20);
+            this.listViewRes.Name = "listViewRes";
+            this.listViewRes.Size = new System.Drawing.Size(343, 159);
+            this.listViewRes.TabIndex = 6;
+            this.listViewRes.UseCompatibleStateImageBehavior = false;
+            this.listViewRes.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "序号";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 40;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "端口";
+            this.columnHeader1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "状态";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 130;
+            // 
+            // buttonUdp
+            // 
+            this.buttonUdp.Location = new System.Drawing.Point(223, 304);
+            this.buttonUdp.Name = "buttonUdp";
+            this.buttonUdp.Size = new System.Drawing.Size(75, 23);
+            this.buttonUdp.TabIndex = 16;
+            this.buttonUdp.Text = "Udp Scan";
+            this.buttonUdp.UseVisualStyleBackColor = true;
+            this.buttonUdp.Click += new System.EventHandler(this.buttonUdpStart_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listViewRes);
+            this.groupBox1.Location = new System.Drawing.Point(22, 87);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(377, 199);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Port Status";
+            // 
+            // buttonTcp
+            // 
+            this.buttonTcp.Location = new System.Drawing.Point(100, 304);
+            this.buttonTcp.Name = "buttonTcp";
+            this.buttonTcp.Size = new System.Drawing.Size(75, 23);
+            this.buttonTcp.TabIndex = 0;
+            this.buttonTcp.Text = "Tcp Scan";
+            this.buttonTcp.UseVisualStyleBackColor = true;
+            this.buttonTcp.Click += new System.EventHandler(this.buttonTcp_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "From:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(208, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 12);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "To:";
+            // 
+            // UDPScan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 354);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbend);
+            this.Controls.Add(this.tbstart);
+            this.Controls.Add(this.tbipFrom);
+            this.Controls.Add(this.buttonUdp);
+            this.Controls.Add(this.buttonTcp);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(437, 393);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(437, 393);
+            this.Name = "UDPScan";
+            this.Text = "Portscan";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UDPScan_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbend;
+        private System.Windows.Forms.TextBox tbstart;
+        private System.Windows.Forms.TextBox tbipFrom;
+        private System.Windows.Forms.ListView listViewRes;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button buttonUdp;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonTcp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ UDPScan/UDPScan.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+namespace UDPScan
+{
+    public partial class UDPScan : Form
+    {
+        IPAddress IP;
+        //主线程
+        private Thread tdm;
+        //开始端口，结束端口
+        int Start, End;
+        //线程数
+        private int count = 0;
+        //当前扫描端口
+        private int i;
+        //Delegate数据结构定义
+        public delegate void DelegateFunction(int status, int i);
+        private DelegateFunction m_delegateFunction;
+        //按钮显示委托操作
+        private delegate void DelegateButton(Button but);
+        private DelegateButton but_delegate;
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public UDPScan()
+        {
+            InitializeComponent();
+            //当前要扫描的ＩＰ地址
+            string HostName = Dns.GetHostName(); //得到主机名           
+            IPHostEntry IpEntry = Dns.GetHostEntry(HostName); //得到主机IP 
+            int ii = 0;
+            for (; ii < IpEntry.AddressList.Length; ii++)
+            {
+                if (IpEntry.AddressList[ii].AddressFamily == AddressFamily.InterNetwork)
+                    break;
+            }
+            tbipFrom.Text = IpEntry.AddressList[ii].ToString();
+            this.m_delegateFunction = new DelegateFunction(subFunctionUDP);
+            this.but_delegate = new DelegateButton(but_Protect);
+        }
+
+        private void but_Protect(Button but)
+        {
+            but.Enabled = true;
+        }
+
+        /// <summary>
+        /// tcp scan
+        /// </summary>
+        private void buttonTcp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IP = IPAddress.Parse(tbipFrom.Text);
+            }
+            catch
+            {
+                MessageBox.Show("请输入合法地址");
+                return;
+            }
+            buttonTcp.Enabled = false;
+            listViewRes.Items.Clear();
+            Thread tmain = new Thread(new ThreadStart(MainProcessTCP));
+            tmain.Start();
+        }
+
+
+        /// <summary>
+        /// udp scan
+        /// </summary>
+        private void buttonUdpStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IP = IPAddress.Parse(tbipFrom.Text);
+            }
+            catch
+            {
+                MessageBox.Show("请输入合法地址");
+                return;
+            }
+            buttonUdp.Enabled = false;
+            listViewRes.Items.Clear();
+            Thread tmain = new Thread(new ThreadStart(MainProcessUDP));
+            tmain.Start();
+        }
+
+        //tcp 主线程
+        #region
+        private void MainProcessTCP()
+        {
+            count = 0;
+            Start = Convert.ToInt32(tbstart.Text);
+            End = Convert.ToInt32(tbend.Text);
+
+            //循环取副线程扫描端口
+            for (i = Start; i <= End; i++)
+            {
+                count++;
+
+                //tdm.Start();
+
+                ParameterizedThreadStart ParStart = new ParameterizedThreadStart(ProcessTCP);
+
+                tdm = new Thread(ParStart);
+                object o = i;
+
+                tdm.Start(o);
+
+                Thread.Sleep(10);
+                //线程数控制在２０
+                while (count > 20) ;
+
+            }
+            while (count > 0) ;
+            MessageBox.Show("检测完成！");
+            this.Invoke(but_delegate, buttonTcp);
+        }
+        #endregion
+
+        /**/
+        /// <summary>
+        /// tcp 副线程
+        /// </summary>
+        #region
+        private void ProcessTCP(object i)
+        {
+            //当前要扫描的ＩＰ地址
+
+            int port = (int)i;
+            TcpClient tc = new TcpClient();
+
+            try
+            {
+                tc.SendTimeout = tc.ReceiveTimeout = 20000;
+                //建立连接
+                tc.Connect(IP, port);
+
+                //判断是否连接成功
+                if (tc.Connected)
+                {
+
+                    this.Invoke(m_delegateFunction, 0, port);
+
+                }
+                else
+                {
+                    this.Invoke(m_delegateFunction, 1, port);
+                }
+
+            }
+            catch (SocketException ex)
+            {
+                if (ex.ErrorCode == 10061)
+                {
+                    this.Invoke(m_delegateFunction, 1, port);
+                }
+            }
+            finally
+            {
+                if (tc != null)
+                {
+                    tc.Close();
+                    tc = null;
+                }
+
+                //修改线程数
+                count--;
+            }
+        }
+        #endregion
+
+
+        //udp 主线程
+        #region
+        private void MainProcessUDP()
+        {
+            count = 0;
+            Start = Convert.ToInt32(tbstart.Text);
+            End = Convert.ToInt32(tbend.Text);
+            //循环取副线程扫描端口
+            for (i = Start; i <= End; i++)
+            {
+                count++;
+                ParameterizedThreadStart ParStart = new ParameterizedThreadStart(ProcessUDP);
+                tdm = new Thread(ParStart);
+                object o = i;
+                tdm.Start(o);
+                Thread.Sleep(10);
+                //线程数控制在２０
+                while (count > 1) ;
+            }
+            while (count > 0) ;
+            MessageBox.Show("检测完成！");
+            this.Invoke(but_delegate, buttonUdp);
+        }
+        #endregion
+
+        /**/
+        /// <summary>
+        /// udp 副线程
+        /// </summary>
+        #region
+        private void ProcessUDP(object i)
+        {
+            //当前要扫描的ＩＰ地址
+            string HostName = Dns.GetHostName(); //得到主机名           
+            IPHostEntry IpEntry = Dns.GetHostEntry(HostName); //得到主机IP 
+            int ii = 0;
+            //得到IPv4
+            for (; ii < IpEntry.AddressList.Length; ii++)
+            {
+                if (IpEntry.AddressList[ii].AddressFamily == AddressFamily.InterNetwork)
+                    break;
+            }
+            //得到本机IPV4地址
+            string strIPAddr = IpEntry.AddressList[ii].ToString();
+            //拆箱
+            int port = (int)i;
+            //得到IP地址
+            IP = IPAddress.Parse(strIPAddr);
+            EndPoint ep = null;
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            //管理员方式才能正确执行此句代码  需要管理员权限
+            Socket icmp = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
+            byte[] data = Encoding.ASCII.GetBytes("hello,jun");
+            IPEndPoint ipe = new IPEndPoint(IP, port);
+            ep = (EndPoint)ipe;
+            try
+            {
+                //绑定网络地址
+                icmp.Bind(ep);
+                //将指定的 System.Net.Sockets.Socket 选项设置为指定的整数值。
+                icmp.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 500);
+                ipe.Address = IPAddress.Parse(tbipFrom.Text);
+                ep = (EndPoint)ipe;
+                sock.SendTo(data, ep);//先发送数据包
+
+                data = new byte[1024];
+                byte[] message = new byte[1024];
+
+                int len = icmp.ReceiveFrom(data, ref ep);//接收ICMP包
+                byte type = data[20];
+                byte code = data[21];
+                UInt16 check = BitConverter.ToUInt16(data, 22);
+                int messagesize = len - 24;
+                Buffer.BlockCopy(data, 24, message, 0, messagesize);
+                this.Invoke(m_delegateFunction, 1, port);
+            }
+            catch (SocketException)//如果没收到ICMP，认为端口开放
+            {
+                this.Invoke(m_delegateFunction, 0, port);
+            }
+            //修改线程数
+            count--;
+        }
+        #endregion
+        /// <summary>
+        /// 委托函数 显示扫描结果
+        /// </summary>
+        #region
+        void subFunctionUDP(int status, int port)
+        {
+            ListViewItem item = new ListViewItem(listViewRes.Items.Count.ToString());
+            item.SubItems.Add(port.ToString());
+            if (status == 0)
+            {
+                item.SubItems.Add("open");
+            }
+            else
+                item.SubItems.Add("close");
+            listViewRes.Items.Add(item);
+        }
+        #endregion
+        /// <summary>
+        /// 窗口退出提示
+        /// </summary>
+        #region
+        private void UDPScan_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("确定退出吗？", "退出", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+                return;
+            }
+        }
+        #endregion
+    }
+}
